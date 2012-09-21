9875e050d8f1d80ae27bb90f3c51004e902e2ece
Fermin Suero <fsuero@yahoo.com>
2012-09-20 22:26:33 -0400

    initial upload

---
+++ main.c
@@ -0,0 +1,82 @@
+/* 
+ * File:   main.c
+ * Author: suerof
+ *
+ * Created on September 20, 2012, 1:57 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <fahr.c>
+
+/*
+ * Main will run all exercises. 
+ */
+int main(int argc, char** argv) {
+
+    exercise1_4();
+    return (EXIT_SUCCESS);
+}
+
+int exercise1_4()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+    
+    lower = 0;          /* lower limit of temp scale */
+    upper = 100;        /* upper limit */
+    step = 10;          /* step size */
+    
+    celsius = lower;
+    printf("Celsius   Fahr\n-----------------\n");
+    while (celsius < upper)
+    {
+        fahr = (celsius * 9.0 / 5.0) + 32;
+        printf("%3.0f\t%6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+    
+}
+
+int exercise1_3()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+    
+    lower = 0;          /* lower limit of temperature scale */
+    upper = 300;        /* upper limit */
+    step = 20;          /* step size */
+    
+    fahr = lower;
+    printf("Fahr       Cel\n-----------------\n");
+    
+    while (fahr <= upper)
+    {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
+
+int exercise1_2()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+    
+    lower = 0;          /* lower limit of temperature scale */
+    upper = 300;        /* upper limit */
+    step = 20;          /* step size */
+    
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
+
+int exercise1_1()
+{
+    printf("hello, world!");
+}
